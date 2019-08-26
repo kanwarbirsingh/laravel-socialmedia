6f5aae89a3db889bf13a6da88c253da344f56a80
Kanwarbir Singh <singhkanwar235@gmail.com>
2019-08-26 15:17:46 -0400

    Initial Commit-Complete till 6 videos of series

---
+++ app/User.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Auth\Authenticatable as IlluminateAuthenticatable;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model implements Authenticatable
+{
+    use IlluminateAuthenticatable;
+}
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['middleware' => ['web']], function () {
+    Route::get('/', function () {
+        return view('welcome');
+    })->name('home');
+    Route::post('/signup', [
+        'uses' => 'UserController@postSignUp',
+        'as' => 'signup'
+    ]);
+    Route::get('/dashboard', [
+        'uses'=> 'UserController@getDashboard',
+        'as' => 'dashboard',
+        'middleware' => 'auth'
+    ]);
+    Route::post('/signin', [
+        'uses' => 'UserController@postSignIn',
+        'as' => 'signin'
+    ]);
+});
